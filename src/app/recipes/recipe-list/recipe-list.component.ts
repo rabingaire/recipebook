0b22d9c5af1fc170600df3bb20db770d874d7955
Rabin Gaire <rabingaire20@gmail.com>
2018-08-13 15:00:54 +0545

    basic course completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'this is simply a test',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT3mEDbLBHhkkqpmhkgwnCTxiIkFp0NrUgCj3TtH5Czj6g3z0n'
+    ),
+    new Recipe(
+      'A test recipe two',
+      'this is simply a test',
+      'https://cookieandkate.com/images/2017/03/strawberry-salsa-recipe.jpg'
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
